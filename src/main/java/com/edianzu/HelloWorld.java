7c69b93e4363805178ca6f8e97358af914d0fcab
zhanyayun <767889590@qq.com>
2018-09-29 09:54:44 +0800

    添加了两个新的功能

--- src/main/java/com/edianzu/HelloWorld.java
+++ src/main/java/com/edianzu/HelloWorld.java
@@ -7,5 +7,7 @@ package com.edianzu;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld1");
+        System.out.println("HelloWorld2");
+        System.out.println("HelloWorld3");
     }
 }
